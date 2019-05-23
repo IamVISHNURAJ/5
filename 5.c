fdc027fb290cb49be766147185ba35b398fd307b
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 13:25:55 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -4,10 +4,10 @@ int main()
     double n1, n2, n3;
     scanf("%lf %lf %lf", &n1, &n2, &n3);
     if( n1>=n2 && n1>=n3)
-     printf("%lf", n1);
+     printf("%.lf", n1);
     else if (n2>=n1 && n2>=n3)
-        printf("%lf", n2);
+        printf("%.lf", n2);
     else
-        printf("%lf", n3);
+        printf("%.lf", n3);
     return 0;
 }
